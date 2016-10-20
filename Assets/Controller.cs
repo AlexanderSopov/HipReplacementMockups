c9792a1e1a05888c1d4ae91234d0647856659537
sopovalexander@gmail.com <sopovalexander@gmail.com>
2016-10-20 11:04:47 +0200

    added controllers

---
+++ Assets/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+    public float moveSpeed = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("s"))
+            transform.Translate(0, Time.deltaTime * moveSpeed * -1, 0);
+        if (Input.GetKey("w"))
+            transform.Translate(0, Time.deltaTime * moveSpeed, 0);
+
+    }
+}
