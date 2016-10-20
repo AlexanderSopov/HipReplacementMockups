233e87df782395d937dbc09a9d05806a96c4f0dd
sopovalexander@gmail.com <sopovalexander@gmail.com>
2016-10-20 17:54:09 +0200

    added rotation comparator in approver.cs

--- Assets/Approver.cs
+++ Assets/Approver.cs
@@ -30,7 +30,11 @@ public class Approver : MonoBehaviour {
 
     private bool withinRange()
     {
-        Vector3 distance = transform.position - stick.transform.position;
-        return distance.magnitude < 0.2f;
+        Vector3 distanceVector = transform.position - stick.transform.position;
+        Vector3 rotationVector =new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z)
+            - new Vector3(stick.transform.rotation.x, stick.transform.rotation.y, stick.transform.rotation.z);
+
+        Debug.Log(rotationVector.magnitude);
+        return distanceVector.magnitude < 0.2f && rotationVector.magnitude < 0.1f;
     }
 }
