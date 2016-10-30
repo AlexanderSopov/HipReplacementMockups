11a9849c63b371ced8ed880a41678e1b71c27aaa
Alexander Sopov <sopovalexander@gmail.com>
2016-10-30 18:21:29 +0100

    started rotation implementation

---
+++ Assets/Scripts/Hololens/Object Manipulator/XYRotatorManipulator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class XYRotatorManipulator : RotatorManipulator
+{
+    internal override void rotate(Vector3 initialToCurrentPosition, Vector3 scaledLocalPositionDelta)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Hololens/Object Manipulator/XZRotatorManipulator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class XZRotatorManipulator : RotatorManipulator
+{
+    internal override void rotate(Vector3 initialToCurrentPosition, Vector3 scaledLocalPositionDelta)
+    {
+
+    }
+}
