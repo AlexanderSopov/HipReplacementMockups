51290bb1de37f2bfb907180fba3b09fb804e828c
sopovalexander@gmail.com <sopovalexander@gmail.com>
2016-11-02 15:52:23 +0100

    tried to fix performance issues to no avail...

--- Assets/Scripts/Desktop/CameraMovement.cs
+++ Assets/Scripts/Desktop/CameraMovement.cs
@@ -14,8 +14,8 @@ public class CameraMovement : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = true;
     }
 
     // Update is called once per frame
